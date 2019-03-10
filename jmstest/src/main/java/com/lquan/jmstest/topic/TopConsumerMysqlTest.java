1b11b80ccbb67306f171da2de2658f8bd0a29f0d
1570599338@qq.com <1570599338@qq.com>
2019-03-11 01:54:16 +0800

    发布订阅模式

---
+++ jmstest/src/main/java/com/lquan/jmstest/topic/TopConsumerMysqlTest.java
@@ -0,0 +1,62 @@
+package com.lquan.jmstest.topic;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.jms.TopicSubscriber;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * 消息的消费者,接收方
+ * @author lquan
+ *
+ */
+public class TopConsumerMysqlTest {
+
+	public static void main(String[] args) throws Exception {
+		// 创建连接工厂
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://112.125.90.15:61616");
+		//2、创建连接
+		Connection connection = connectionFactory.createConnection();
+		
+		//4、设置客户端的id
+		connection.setClientID("clientlquan-1");
+		//3、启动连接
+		connection.start();
+		// 5、获取session
+		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		// 6、通过session创建主题
+		Topic topic = session.createTopic("lquanTic");
+		// 7、同过session对象创建持久化消息的消费者
+		TopicSubscriber consumer = session.createDurableSubscriber(topic, "client1-sub");
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				if(message instanceof TextMessage) {
+					
+					TextMessage textMessage = (TextMessage)message;
+					try {
+		                System.out.println("接收到消息："+textMessage.getText());
+		            } catch (JMSException e) {
+		                e.printStackTrace();
+		            }
+				}
+				
+			}
+		});
+		
+		
+		while (true){
+
+        }
+
+	}
+
+}
---
+++ jmstest/src/main/java/com/lquan/jmstest/topic/TopicPersisitenMysqlTest.java
@@ -0,0 +1,52 @@
+package com.lquan.jmstest.topic;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * 持久化mq的消息
+ * @author lquan
+ *
+ */
+public class TopicPersisitenMysqlTest {
+
+	public static void main(String[] args) throws Exception {
+		// 创建工厂
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://112.125.90.15:61616");
+		// 创建连接
+		Connection connection=connectionFactory.createConnection();
+		// 开启连接
+		connection.start();
+		// 创建会话session
+		/**
+		 * createSession 
+		 * 参数1:是否开启事务
+		 * 参数2:签收模式,一般我们使用自动签收模式
+		 */
+		Session session=connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		// 创建主题
+		Topic topic=session.createTopic("lquanTic");
+		// 创建一个发布者
+		MessageProducer producer = session.createProducer(topic);
+		// 持久化消息
+		producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+		// 创建一条消息
+		TextMessage message = session.createTextMessage("订阅消息");
+		// 发送消息
+		//producer.send(message,DeliveryMode.PERSISTENT,1,1000*60*60*24);
+		producer.send(message);
+		
+		producer.close();
+		session.close();
+		connection.close();
+	}
+
+}
