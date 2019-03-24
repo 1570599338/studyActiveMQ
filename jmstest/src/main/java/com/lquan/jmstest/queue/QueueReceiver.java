93cb795e09cde33b7690ca8b17384956ce105034
1570599338@qq.com <1570599338@qq.com>
2019-03-24 23:49:22 +0800

    优化代码

--- jmstest/src/main/java/com/lquan/jmstest/queue/QueueReceiver.java
+++ jmstest/src/main/java/com/lquan/jmstest/queue/QueueReceiver.java
@@ -3,11 +3,7 @@ package com.lquan.jmstest.queue;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
 import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
